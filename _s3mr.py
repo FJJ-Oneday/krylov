ec17d0eb1a4d1e34593b9a71a19c0098f1256691
Fan <fjj1958365778@163.com>
2023-07-03 22:48:34 +0800

    add block s3mr method

--- _s3mr.py
+++ _s3mr.py
@@ -50,7 +50,130 @@ def s3mr(S, b, alpha, /, tol: float=1e-6, maxit: int=None, x0=None) -> tuple[np.
 
         gamma1 = gamma2
         w0, w1 = w1, w / gamma1
-        p_1, p0 = p0, p_1
+        p_1, p0 = p0, p
         c0, s_1, s0 = c1, s0, s1
 
     return x, {'exitflag': flag, 'iters': j + 1, 'resvec': resvec}
+
+
+def bls3mr(S, B, alpha, /, tol: float=1e-6, maxit: int=None, X0=None) -> tuple[np.array, dict]:
+    n, p = B.shape
+
+    if not maxit:
+        maxit = n
+    
+    if not X0:
+        X0 = np.zeros((n, p))
+    X = X0
+    
+    if not callable(S):
+        S_Mut = lambda x: S.dot(x)
+    else:
+        S_Mut = S
+    
+    R0 = B - (S_Mut(X0) + alpha * X0)
+    W1, R = linalg.qr(R0, mode='economic')
+    norm_r = linalg.norm(R)
+    H1 = G_tilde = R
+    W0 = P_1 = P0 = 0
+
+    C_1, S_1, C0, S0 = [np.zeros((p, p)) for _ in range(4)]
+    Zeros, AlphaI = np.zeros((p, p)), alpha * np.eye(p)
+    
+
+    flag, resvec = 1, np.array([norm_r])
+    for j in range(maxit):
+        if resvec[-1] / norm_r < tol:
+            flag = 0
+            break
+
+        if j == 0:
+            W = S @ W1
+        else:
+            W = S @ W1 + W0 @ H1.T
+        W2, H2 = linalg.qr(W, mode='economic')
+
+        if j > 1:
+            T = np.vstack( (Zeros, -H1.T) )
+            T = _blrotation_of_s3mr(T, C_1, S_1)
+            # for i in range(p):
+            #     for k in range(p):
+            #         index = p + i - k - 1
+            #         c, s = C_1[k, i], S_1[k, i]
+            #         T[index:index+2, :] = np.array([[c, s], [-s, c]]) @ T[index:index+2, :]
+            Phi, Theta = T[:p, :], T[p:, :]
+        
+        if j == 1:
+            Theta = -H1.T
+
+        if j > 0:
+            T = np.vstack( (Theta, AlphaI) )
+            T = _blrotation_of_s3mr(T, C0, S0)
+            # for i in range(p):
+            #     for k in range(p):
+            #         index = p + i - k - 1
+            #         c, s = C0[k, i], S0[k, i]
+            #         T[index:index+2, :] = np.array([[c, s], [-s, c]]) @ T[index:index+2, :]
+            Omga_tilde = T[p:, :]
+        
+        if j == 0:
+            Omga_tilde = alpha * np.eye(p)
+        
+        C_1, S_1 = C0.copy(), S0.copy()
+
+        T = np.vstack( (Omga_tilde, H2) )
+        T1 = np.vstack( (G_tilde, Zeros) )
+        T, T1 = _blrotation_of_s3mr(T, C0, S0, T1)
+        # for i in range(p):
+        #     for k in range(p):
+        #         index = p + i - k -1
+
+        #         delta_tilde, gamma = T[index:index+2, i]
+        #         delta = np.sqrt( delta_tilde ** 2 + gamma ** 2 )
+        #         c, s = delta_tilde / delta, gamma / delta
+        #         C0[k, i], S0[k, i] = c, s
+
+        #         Rot_Mat = np.array([[c, s], [-s, c]])
+        #         T[index:index+2, :] = Rot_Mat @ T[index:index+2, :]
+        #         T1[index:index+2, :] = Rot_Mat @ T1[index:index+2, :]
+
+        Omega, G, G_tilde = T[:p, :], T1[:p, :], T1[p:, :]
+        resvec = np.append(resvec, linalg.norm(G_tilde))
+
+        if j < 2:
+            PT = linalg.solve_triangular(Omega, W1.T, trans='T')
+        else:
+            PT = linalg.solve_triangular(Omega, (W1 - P_1 @ Phi).T, trans='T')
+
+        P = PT.T
+        X = X + P @ G
+
+        W0, W1 = W1, W2
+        P_1, P0 = P0, P
+        H1 = H2
+    
+    return X, {'exitflag': flag, 'iters': j + 1, 'resvec': resvec}
+
+
+def _blrotation_of_s3mr(A, C, S, /, B=None) -> np.ndarray:
+    p = C.shape[0]
+    for i in range(p):
+        for k in range(p):
+            index = p+i-k-1
+
+            if B is not None:
+                delta_tilde, gamma = A[index:index+2, i]
+                delta = np.sqrt( delta_tilde ** 2 + gamma ** 2 )
+                c, s = delta_tilde / delta, gamma / delta
+                C[k, i], S[k, i] = c, s
+            else:
+                c, s = C[k, i], S[k, i]
+            
+            Rot_Mat = np.array([[c, s], [-s, c]])
+            A[index:index+2, :] = Rot_Mat @ A[index:index+2, :]
+            if B is not None:
+                B[index:index+2, :] = Rot_Mat @ B[index:index+2, :]
+    
+    if B is not None:
+        return A, B
+    return A
